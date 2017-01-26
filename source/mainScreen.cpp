687b4e590d9f05618004e995836503cedc43e037
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2017-01-26 11:00:11 +0000

    Update mainScreen.cpp

--- source/mainScreen.cpp
+++ source/mainScreen.cpp
@@ -5,7 +5,11 @@ void mainScreen::init(ds_GFX gfx){
 }
 
 void mainScreen::input(ds_GFX gfx){
-
+	if (hidkeydown() & KEY_A){
+		bg.r = rand() % 255;
+		bg.g = rand() % 255;
+		bg.b = rand() % 255;
+	}
 }
 
 void mainScreen::update(ds_GFX gfx){
@@ -14,8 +18,5 @@ void mainScreen::update(ds_GFX gfx){
 
 void mainScreen::draw(ds_GFX gfx){
 	gfx.beginFrame(GFX_TOP);
-	gfx.clear((ds_Col){0,0,0});
-	gfx.rectFill((ds_Rect){0,0,5,5},(ds_Col){255,0,0});
-	gfx.rectFill((ds_Rect){0,0,2,2},(ds_Col){0,255,0});
-	gfx.rectFill((ds_Rect){0,0,1,1},(ds_Col){0,0,255});
+	gfx.clear(bg);
 }
