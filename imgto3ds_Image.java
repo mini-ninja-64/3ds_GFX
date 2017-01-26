af6bf8f322a63d2388222594576a7fd1b61aafe7
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2017-01-26 20:07:56 +0000

    Create imgto3ds_Image.java

---
+++ imgto3ds_Image.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("C:\\Users\\callum\\Desktop\\space_xd.png"));
+        } catch (IOException e) {
+        }
+
+        int w = img.getWidth();
+        int h = img.getHeight();
+
+        byte[] fileBuff = new byte[8+(3*w*h)];
+
+        byte[] width = new byte[4];
+        byte[] height = new byte[4];
+        width =  ByteBuffer.allocate(4).putInt(w).array();
+        height =  ByteBuffer.allocate(4).putInt(h).array();
+
+        fileBuff[0] = width[0]; fileBuff[1] = width[1]; fileBuff[2] = width[2]; fileBuff[3] = width[3];
+        fileBuff[4] = height[0]; fileBuff[5] = height[1]; fileBuff[6] = height[2]; fileBuff[7] = height[3];
+
+        for (int x = 0; x < w; x++){
+            for (int y = 0; y < h; y++){
+                int pitch =  w * (1 * 3);
+                int pos = (y * pitch) + (x * 1 * 3);
+                int rgb = img.getRGB(x,y);
+                fileBuff[8+pos+2] = (byte) ((rgb & 0x00ff0000) >> 16);
+                fileBuff[8+pos+1] = (byte) ((rgb & 0x0000ff00) >> 8);
+                fileBuff[8+pos+0] = (byte) (rgb & 0x000000ff);
+            }
+        }
+        FileOutputStream fos = new FileOutputStream("C:\\Users\\callum\\Desktop\\space_xd.3ds_Image");
+        fos.write(fileBuff);
+        fos.close();
+    }
+}
